cb97edc2e0be37bb097d9a056b66bce358f5a61a
SAMAIRR <133367771+SAMAIRR@users.noreply.github.com>
2024-10-24 17:44:43 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let a1=document.getElementById("a1")
+let a2=document.getElementById("a2")
+let a3=document.getElementById("a3")
+
+a2.addEventListener("click",function(){
+a1.innerHTML="<h1>salam</h1>"
+a3.style.right="23.5rem"
+})
