d1736e8116eacbef4a02a17e2691cf1515a03ac5
SAMAIRR <133367771+SAMAIRR@users.noreply.github.com>
2024-10-25 13:54:07 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-let a1=document.getElementById("a1")
-let a2=document.getElementById("a2")
-let a3=document.getElementById("a3")
+let padcast=document.getElementById("padcast")
+let zir=document.getElementById("zir")
+let home=document.getElementById("home")
+let ketab=document.getElementById("ketab")
+let talar=document.getElementById("talar")
+let dore=document.getElementById("dore")
 
-a2.addEventListener("click",function(){
-a1.innerHTML="<h1>salam</h1>"
-a3.style.right="23.5rem"
+padcast.addEventListener("click",()=>{
+   zir.style.right="5.8rem"
 })
-
-let meno =document.getElementById("meno-navbar")
-
-
-meno.addEventListener("click",()=>{
-    alert(111)
-})
+home.addEventListener("click",()=>{
+    zir.style.right="11.8rem"
+ })
+ dore.addEventListener("click",()=>{
+    zir.style.right="0.8rem"
+ })
+ ketab.addEventListener("click",()=>{
+    zir.style.right="17.8rem"
+ })
+ talar.addEventListener("click",()=>{
+    zir.style.right="23.8rem"
+ })
